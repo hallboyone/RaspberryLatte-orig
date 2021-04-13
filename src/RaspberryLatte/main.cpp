0a825d39dcd2d804f0c2058b9cf2074582807a9a
Richard Hall <hallboyone@icloud.com>
2021-04-13 16:34:10 +0100

    Updated include in main so emacs could find them.

--- src/RaspberryLatte/main.cpp
+++ src/RaspberryLatte/main.cpp
@@ -1,15 +1,14 @@
-#include "BinarySensor.hpp"
-#include "MAX31855.hpp"
+#include "../../include/RaspberryLatte/BinarySensor.hpp"
+#include "../../include/RaspberryLatte/MAX31855.hpp"
 #include <iostream>
-
 
 int main(void){
   RaspLatte::PinIndex gpio_num = 0;
   bool pull_down = false;
   bool invert = false;
   RaspLatte::BinarySensor sensor1(gpio_num, pull_down, invert);
-
   RaspLatte::MAX31855 tempSensor(0);
+  
   double temp = tempSensor.read();
   if(temp == MAX31855_TEMP_UNAVALIBLE){
     tempSensor.printError();
@@ -20,5 +19,4 @@ int main(void){
   
   std::cout<<"Sensor 1: "<<sensor1.read()<<std::endl;
   return 1;
-}
-  
+}  

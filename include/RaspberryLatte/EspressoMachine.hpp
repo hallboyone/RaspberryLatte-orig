c6d725ee15789077574cd8204126816b9f4c811b
Richard Hall <hallboyone@icloud.com>
2021-04-27 20:57:57 -0400

    Added comments to the EspressoMachine header.

--- include/RaspberryLatte/EspressoMachine.hpp
+++ include/RaspberryLatte/EspressoMachine.hpp
@@ -13,13 +13,10 @@ namespace RaspLatte{
 
   class EspressoMachine{
   private:
-    
     TempPair temps_;
     
     MAX31855 boiler_temp_sensor_;
-    
     Boiler boiler_;
-
     RaspberryLatteUI ui_;
     
     Switch pwr_switch_;
@@ -27,19 +24,45 @@ namespace RaspLatte{
     Switch steam_switch_;
 
     MachineMode current_mode_;
-        
-    bool atSetpoint();
+
+    /*
+     * Update the current mode's setpoint by the increment. If mode is off, do nothing
+     */
     void updateSetpoint(double increment);
+
+    /*
+     * Use the current state of the power and steam switch to get the curreent mode of the system
+     */
     void updateMode();
+
+    /*
+     * Use the current mode and tempurature to turn on and off the lights
+     */
     void updateLights();
+
+    /*
+     * Update internal paarameters based on the value of key
+     */
     void handleKeyPress(int key);
     
   public:
     EspressoMachine(double brew_temp, double steam_temp);
+
+    /*
+     * Runs a loop where the UI is refreshed, any keys are handled, and the 
+     * boiler is updated until 'q' is pressed. 
+     */
     void run();
+
+    /*
+     * Getters
+     */
     MachineMode currentMode();
     bool pumpOn();
     double setpoint();
+    bool atSetpoint();
+
+    
     ~EspressoMachine();
   };
 }
